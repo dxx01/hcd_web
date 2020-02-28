d244ad1b23e98375838db53406288e64c8bd95f4
Mr.Deng <1693172461@qq.com>
2020-02-28 18:38:06 +0800

    用户父模板

---
+++ src/views/User.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="lrf-user">
+    <div class="lrf-logo">HCD</div>
+    <!-- 路由占位符-->
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less" scoped>
+#lrf-user {
+  background-color: #f3f3f3;
+  height: 100%;
+}
+.lrf-logo {
+  width: 30%;
+  min-width: 400px;
+  height: 100px;
+  font-size: 60px;
+  font-weight: bolder;
+  position: absolute;  /* 绝对定位 */
+  left: 50%;
+  right: 50%;
+  transform: translate(-50%, 40px);
+  text-align: center;
+  line-height: 100px;
+}
+</style>
