f72810639f9042c905a443945db3f6537ce66dd4
Mr.Deng <1693172461@qq.com>
2020-04-02 22:05:44 +0800

    ueditor编辑器模块

---
+++ src/components/Editor.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="UE">
+        <vue-ueditor-wrap v-model="value" :config="config"></vue-ueditor-wrap>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'UE',
+  data () {
+    return {
+      value: '这是我调用vue-ueditor-wrap的案例',
+      config: {
+        // 初始容器高度
+        initialFrameHeight: '100%',
+        // 初始容器宽度
+        initialFrameWidth: '100%',
+        serverUrl: 'http://35.201.165.105:8000/controller.php',
+        UEDITOR_HOME_URL: '/UE/'
+      }
+    }
+  }
+}
+</script>
